bef7cd317bd123ce037335ead19cf6623ee12506
tegarmarino <tmarino01@student.ciputra.ac.id>
2023-04-11 08:50:48 +0700

    Apple SwiftUI Tutorial - Section 3

--- 0706012110006-TegarMarino-AFL3/ContentView.swift
+++ 0706012110006-TegarMarino-AFL3/ContentView.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle rock")
-            .font(.title)
-            .foregroundColor(.green)
+        VStack(alignment: .leading) {
+            Text("Turtle rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
