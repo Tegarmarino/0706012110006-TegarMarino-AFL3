88ab3b2d9c806e2edc729ba6d14b9173f37f3f8e
tegarmarino <tmarino01@student.ciputra.ac.id>
2023-04-11 08:19:15 +0700

    Initial Commit

---
+++ 0706012110006-TegarMarino-AFL3/_706012110006_TegarMarino_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110006_TegarMarino_AFL3App.swift
+//  0706012110006-TegarMarino-AFL3
+//
+//  Created by Tegar Marino on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110006_TegarMarino_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
