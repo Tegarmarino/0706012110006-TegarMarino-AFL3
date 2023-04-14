fb60c517a9de28c062440e0623e0ee9a9196f3b0
tegarmarino <tmarino01@student.ciputra.ac.id>
2023-04-14 09:54:49 +0700

    Chapter 2 - Bagian 1 - Section 3

---
+++ 0706012110006-TegarMarino-AFL3/RotatedBadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  RotatedBadgeSymbol.swift
+//  0706012110006-TegarMarino-AFL3
+//
+//  Created by Tegar Marino on 14/04/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
