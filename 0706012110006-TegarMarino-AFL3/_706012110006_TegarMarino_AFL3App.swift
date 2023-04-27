183e8c50908fed7964eed14fa478f4767ccae193
tegarmarino <tmarino01@student.ciputra.ac.id>
2023-04-27 22:33:07 +0700

    Chapter 4 - bagian 3 - Section 7

--- 0706012110006-TegarMarino-AFL3/_706012110006_TegarMarino_AFL3App.swift
+++ 0706012110006-TegarMarino-AFL3/_706012110006_TegarMarino_AFL3App.swift
@@ -16,5 +16,22 @@ struct _706012110006_TegarMarino_AFL3App: App {
             ContentView()
                 .environmentObject(modelData)
         }
+        
+        #if !os(watchOS)
+        .commands {
+            LandmarkCommands()
+        }
+        #endif
+
+
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
     }
 }
